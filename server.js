38ed81d9d0a63bbe3dd931e0a974fc603b013791
RakeshManjunatha21 <rakesh.manjunath21@gmail.com>
2026-03-29 16:51:20 +0530

    cros added allowed headers

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'access-control-allow-methods', 'access-control-allow-headers', 'access-control-allow-origin']
 }));
 
 app.use(express.json());
